5b9666ffbd0125b6457c1d1f0871ae799f07900c
AjayJadhav2151 <ajayjadhav.skn.comp@gmail.com>
2025-01-30 17:47:59 +0530

    changes in javascript file

--- TodoList/script.js
+++ TodoList/script.js
@@ -20,4 +20,60 @@ const mainTodoElem = document.querySelector(".todo-list-elem");
         mainTodoElem.append(divElement);
         };
 
-   
+    const addTodoList = (e) => {
+        e.preventDefault();
+        // console.log("testing");
+
+        const todoListValue = inputValue.value.trim();
+
+        inputValue.value = "";
+         
+        if ( todoListValue !== "" && !localTodoLists.includes(todoListValue)) {
+            localTodoLists.push(todoListValue);
+            localTodoLists = [...new Set(localTodoLists)];
+            console.log(localTodoLists); 
+            localStorage.setItem('youtubeTodoList', JSON.stringify(localTodoLists));   
+            
+            addTodoDynamicElement(todoListValue);
+        }
+        };
+        
+        const showTodoList = () => {
+            console.log(localTodoLists);
+            
+            localTodoLists.forEach((curElem)=> {
+                addTodoDynamicElement(curElem);
+            }); 
+        }
+        showTodoList();
+
+        // remove the data 
+        const removeTodoElem = (e) => {
+            const todoToRemove = e.target;
+            let todoListContent = todoToRemove.previousElementSibling.innerText;
+            let parentElem = todoToRemove.parentElement;
+            console.log(todoListContent);
+            
+            localTodoLists = localTodoLists.filter((curTodo) => {
+                return curTodo !== todoListContent.toLowerCase();
+            });
+            
+            addTodoListLocalStorage(localTodoLists);
+            parentElem.remove();
+
+            console.log(localTodoLists);
+        };
+        
+
+        mainTodoElem.addEventListener("click", (e) =>{
+            e.preventDefault();
+            console.log(e.target.classList.contains("deleteBtn"));
+            if (e.target.classList.contains("deleteBtn")) {
+                removeTodoElem(e);
+            }
+            
+        });
+
+        document.querySelector(".btn").addEventListener("click", (e) => {
+            addTodoList(e);
+        });
